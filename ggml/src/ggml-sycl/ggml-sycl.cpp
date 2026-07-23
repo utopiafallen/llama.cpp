d1979c83e25ffede18d13e63f0cfbeb871d31e94
Todd Malsbary <todd.malsbary@intel.com>
2026-07-23 19:02:12 +0000

    Use ESIMD by default when available
    Signed-off-by: Todd Malsbary <todd.malsbary@intel.com>

--- ggml/src/ggml-sycl/ggml-sycl.cpp
+++ ggml/src/ggml-sycl/ggml-sycl.cpp
@@ -43,6 +43,9 @@
 #    include <sycl/ext/oneapi/virtual_mem/virtual_mem.hpp>
 #    define GGML_SYCL_SUPPORT_VMM
 #endif
+#if defined(__INTEL_LLVM_COMPILER)
+    #define GGML_SYCL_DMMV_HAS_ESIMD
+#endif
 #include <sycl/half_type.hpp>
 
 #include "ggml.h"
@@ -3746,6 +3749,21 @@ inline bool ggml_sycl_supports_reorder_mmvq(enum ggml_type type) {
     }
 }
 
+static bool ggml_sycl_supports_reorder_esimd(enum ggml_type type) {
+#ifdef GGML_SYCL_DMMV_HAS_ESIMD
+    switch (type) {
+        case GGML_TYPE_Q4_K:
+        case GGML_TYPE_Q6_K:
+            return true;
+        default:
+            return false;
+    }
+#else
+    GGML_UNUSED(type);
+    return false;
+#endif
+}
+
 static bool ggml_sycl_supports_dmmv(enum ggml_type type) {
     switch (type) {
         case GGML_TYPE_Q1_0:
@@ -4449,19 +4467,20 @@ static void ggml_sycl_mul_mat(ggml_backend_sycl_context & ctx, const ggml_tensor
     use_mul_mat_q = use_mul_mat_q && (src1->ne[1] <= MMQ_MAX_BATCH_SIZE);
 #endif // SYCL_USE_XMX
 
-    // Dispatch becomes obscure with the reorder, MMVQ when the reorder optimization
-    // is enabled takes precedence over DMMV, the current if-else implementation
-    // requires disabling DMMV if both conditions are met
+    // when reorder is enabled, both ESIMD, MMVQ and DMMV kernels may be used
+    // for best performance use ESIMD when supported, followed by MMVQ, and finally DMMV
 
-    if (!g_ggml_sycl_prioritize_dmmv && ((should_reorder_tensor(ctx, dst) &&
-                                          ggml_sycl_supports_reorder_mmvq(src0->type)))) {
-      // Arc770 get benefit with Q4_0 by skipping it.
-      if (!(ggml_sycl_info().devices[ctx.device].hw_info.arch ==
-                gpu_arch::intel_gpu_acm_g10 &&
-            src0->type == GGML_TYPE_Q4_0)) {
-        use_dequantize_mul_mat_vec =
-            use_dequantize_mul_mat_vec && !use_mul_mat_vec_q;
-      }
+    if (!g_ggml_sycl_prioritize_dmmv && should_reorder_tensor(ctx, dst)) {
+        bool use = g_ggml_sycl_enable_esimd && ggml_sycl_supports_reorder_esimd(src0->type);
+        if (ggml_sycl_supports_reorder_mmvq(src0->type)) {
+            // Arc770 get benefit with Q4_0 by skipping MMVQ path
+            if (!(ggml_sycl_info().devices[ctx.device].hw_info.arch ==
+                        gpu_arch::intel_gpu_acm_g10 &&
+                    src0->type == GGML_TYPE_Q4_0)) {
+                use = use || !use_mul_mat_vec_q;
+            }
+        }
+        use_dequantize_mul_mat_vec = use_dequantize_mul_mat_vec && use;
     }
 
     if (!split && src0->type == GGML_TYPE_F16 && ggml_is_permuted(src0) && ggml_is_permuted(src1) && src1->ne[1] == 1) {
